353f9f8d8a8dc7e5c07bde5b0966ee8bbb66f8f6
takuyaoda <88299141+takuyaod@users.noreply.github.com>
2023-06-13 17:07:36 +0900

    テスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <p>テスト4</p>
+        <p>テスト4.1</p>
         <p>{process.env.NEXT_PUBLIC_TEXT}</p>
 
         <p className={styles.description}>
